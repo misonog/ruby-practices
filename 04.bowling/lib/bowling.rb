49e7f41adcdd5789782aa90ada8e2fac63cd80ad
misono <ginpeimisonou@gmail.com>
2021-08-12 23:26:22 +0900

    スコアリストをフレーム毎のリストに変換するメソッドの追加

--- 04.bowling/lib/bowling.rb
+++ 04.bowling/lib/bowling.rb
@@ -6,3 +6,22 @@
 def parse_score(score)
   score.split(',').map { |s| s == 'X' ? 10 : s.to_i }
 end
+
+# スコアをフレーム毎のリストに変換する
+def convert_to_frames(scores)
+  pin = 10
+  frame = []
+  frames = []
+  scores.each do |score|
+    pin -= score
+    frame << score
+
+    next unless frames.size < 9 && (pin.zero? || frame.size >= 2)
+
+    frames << frame
+    pin = 10
+    frame = []
+  end
+
+  frames << frame
+end
