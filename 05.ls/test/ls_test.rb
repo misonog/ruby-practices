fb8c9895ed12de8dc85e690aee4f382557278aa0
misono <ginpeimisonou@gmail.com>
2021-08-15 17:01:38 +0900

    テストデータのファイル名称変更に伴うテストの修正

--- 05.ls/test/ls_test.rb
+++ 05.ls/test/ls_test.rb
@@ -5,7 +5,7 @@ require './05.ls/lib/ls'
 
 class LSTest < Minitest::Test
   def test_generate_path_list
-    expected = %w[./05.ls/testdata/testdir ./05.ls/testdata/bar.rb ./05.ls/testdata/.hoge ./05.ls/testdata/foo.md ./05.ls/testdata/.. ./05.ls/testdata/.]
+    expected = %w[./05.ls/testdata/testdir ./05.ls/testdata/bar.txt ./05.ls/testdata/.hoge ./05.ls/testdata/foo.md ./05.ls/testdata/.. ./05.ls/testdata/.]
     actual = LS.generate_path_list('./05.ls/testdata')
     assert_equal expected, actual
   end
