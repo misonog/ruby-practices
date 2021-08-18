183e4131ff1148e529819b17760c5d48f9f50bde
misono <ginpeimisonou@gmail.com>
2021-08-18 10:31:53 +0900

    WordCountクラスの追加

--- 06.wc/lib/wc.rb
+++ 06.wc/lib/wc.rb
@@ -4,4 +4,15 @@ module WC
   def self.read_files(paths)
     paths.map { |path| File.readlines(path) }
   end
+
+  class WordCount
+    attr_reader :lines, :words, :bytes, :name
+
+    def initialize(contents, path = '')
+      @lines = contents.size
+      @words = contents.join.split(/\s+/).count { |s| !s.empty? }
+      @bytes = contents.join.bytesize
+      @name = path
+    end
+  end
 end
